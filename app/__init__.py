5843746bd274a6f8a50c968257be5b93df5a99c8
gigadestroyerxd <100698655+gigadestroyerxd@users.noreply.github.com>
2023-06-02 13:11:42 +0200

    Added beans_filter

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from app.beans.routes import bp
 from flask_cors import CORS
+import psycopg2
 
 app = Flask(__name__)
 cors = CORS(app)
 app.register_blueprint(bp)
 
 # Connect to db here
-#db = "dbname='asdf' user='postgres' host='127.0.0.1' password = 'UIS'"
-#conn = psycopg2.connect(db)
+db = "dbname='asdf' user='postgres' host='127.0.0.1' password = 'UIS'"
+conn = psycopg2.connect(db)
+
